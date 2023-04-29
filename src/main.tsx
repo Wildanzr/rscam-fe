2ff576e95258995e11fa74641ad1a4adcdb5b877
wildanzr <graita.sukma@gmail.com>
2023-04-29 18:13:59 +0700

    feat: layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'antd/dist/reset.css'
+
+import { ConfigProvider } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#00B96B',
+        }
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
 )
